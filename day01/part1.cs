c7ac7e6a912b8b58ff25de91e0e6cb3c2e71618b
Maurits van der Schee <maurits@vdschee.nl>
2018-11-25 20:04:39 +0100

    refactor

--- day01/part1.cs
+++ day01/part1.cs
@@ -1,22 +1,19 @@
 using System;
 using System.IO;
 
-namespace Advent.Of
+class Code
 {
-    class Code
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
+        var s = File.ReadAllText("input");
+        int n = 0;
+
+        for (int i = 0; i < s.Length; i++)
         {
-            var s = File.ReadAllText("input");
-            int n = 0;
-
-            for (int i = 0; i < s.Length; i++)
-            {
-                if (s[i] == ')') n--;
-                if (s[i] == '(') n++;
-            }
-
-            Console.WriteLine("{0}", n);
+            if (s[i] == ')') n--;
+            if (s[i] == '(') n++;
         }
+
+        Console.WriteLine("{0}", n);
     }
 }
