dfd4f98f91d1b545cb0fc37f42269aea85461ea6
Maurits van der Schee <maurits@vdschee.nl>
2018-11-25 20:03:20 +0100

    refactor

--- day01/part1.java
+++ day01/part1.java
@@ -2,13 +2,16 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 
-public class Code {
+class Code {
 
 	public static void main(String[] args) throws IOException {
 		String input = new String(Files.readAllBytes(Paths.get("input"))).trim();
 		int n = 0;
 		for (int i = 0; i < input.length(); i++) {
-			n+=(input.charAt(i)=='('?1:(input.charAt(i)==')'?-1:0));
+			switch (input.charAt(i)) {
+				case '(': n++; break;
+				case ')': n--; break;
+			}
 		}
 		System.out.println(n);
 	}
