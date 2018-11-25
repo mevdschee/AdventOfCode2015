fe721f446737e25fe061c9e72056f151b6be7097
Maurits van der Schee <maurits@vdschee.nl>
2018-11-25 14:17:00 +0100

    refactor

--- day01/part1.py
+++ day01/part1.py
@@ -1,11 +1,11 @@
 import sys
 
-floor = 0
+n = 0
 for input in sys.stdin:
     for c in input:
         if c == "(":
-            floor += 1
+            n += 1
         elif c == ")":
-            floor -= 1
+            n -= 1
 
-print(floor)
+print(n)
